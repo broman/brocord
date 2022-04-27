971be3ff1f23994469389a31ccc53dea5ec24cfd
Ryan Broman <ryanjbroman@gmail.com>
2022-04-26 20:22:01 -0700

    Use private fields like a good boy

--- gateway.ts
+++ gateway.ts
@@ -8,7 +8,7 @@ interface Connection {
 
   sendUTF(data: string): void
 
-  on(error: string, param2: (error: any) => void): void
+  on(event: string, param2: (error: any) => void): void
 }
 
 interface Error {
@@ -16,34 +16,34 @@ interface Error {
 }
 
 class Gateway {
-  client: typeof WebSocketClient
-  con?: Connection
-  seq?: number
+  #client: typeof WebSocketClient
+  #con?: Connection
+  #seq?: number
 
   constructor() {
-    this.client = new WebSocketClient()
+    this.#client = new WebSocketClient()
     this.start()
   }
   start() {
-    this.client.on('connectFailed', (error: Error) => {
+    this.#client.on('connectFailed', (error: Error) => {
       logger.error('Connect Error: ' + error.toString())
     })
 
     // Main loop, handles data receiving, errors, and disconnects.
-    this.client.on('connect', (connection: Connection) => {
-      this.con = connection
+    this.#client.on('connect', (connection: Connection) => {
+      this.#con = connection
       logger.info('WebSocket Client Connected')
-      this.con.on('error', function (error) {
+      this.#con.on('error', function (error) {
         logger.error("Connection Error: " + error.toString())
       })
-      this.con.on('close', function (event) {
+      this.#con.on('close', function (event) {
         logger.info(`Connection closed with code ${event}`)
       })
-      this.con.on('message', (message) => {
+      this.#con.on('message', (message) => {
         if (message.type === 'utf8') {
           const payload = JSON.parse(message.utf8Data)
           logger.info(message.utf8Data)
-          this.seq = payload.s
+          this.#seq = payload.s
           switch(payload.op) {
             case Opcodes.DISPATCH:
               // Any and all Discord events are sent here.
@@ -65,20 +65,20 @@ class Gateway {
         }
       })
     })
-    this.client.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+    this.#client.connect('wss://gateway.discord.gg/?v=9&encoding=json')
   }
   sendJSON(payload?: Object) {
     logger.info(`Sending ${JSON.stringify(payload)}`)
-    this.con?.sendUTF(JSON.stringify(payload))
+    this.#con?.sendUTF(JSON.stringify(payload))
   }
   send(payload: string) {
-    this.con?.sendUTF(payload)
+    this.#con?.sendUTF(payload)
   }
 
   heartbeat() {
     this.sendJSON({
       "op": Opcodes.HEARTBEAT,
-      "d": this.seq ?? null
+      "d": this.#seq ?? null
     })
   }
 
